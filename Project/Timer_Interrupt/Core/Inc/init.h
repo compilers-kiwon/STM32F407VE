277261c33cd28ab0bd4800c76665ed2126dd7346
Kiwon Kwon <compilers@naver.com>
2020-08-14 22:57:28 +0900

    An interrupt is enabled on Timer7

---
+++ Project/Timer_Interrupt/Core/Inc/init.h
@@ -0,0 +1,13 @@
+/*
+ * init.h
+ *
+ *  Created on: 2020. 8. 13.
+ *      Author: Kiwon
+ */
+
+#ifndef INC_INIT_H_
+#define INC_INIT_H_
+
+void	LED_Init(void);
+
+#endif /* INC_INIT_H_ */
---
+++ Project/Timer_Interrupt/Core/Src/init.c
@@ -0,0 +1,22 @@
+/*
+ * init.c
+ *
+ *  Created on: 2020. 8. 13.
+ *      Author: Kiwon
+ */
+
+#include	"gpio.h"
+#include	"init.h"
+
+void	LED_Init(void)
+{
+	// Turn off left LED
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_SET);
+
+	// Turn off right LED
+	HAL_GPIO_WritePin(GPIOC,GPIO_PIN_6,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_0,GPIO_PIN_SET);
+}
