8d331183298878fda193e12f97a233e61a3e5b41
Kiwon Kwon <compilers@naver.com>
2020-08-20 23:33:36 +0900

    EXTernal Interrupt from SW1~4 GPIOs

---
+++ Project/EXTI/Core/Inc/util.h
@@ -0,0 +1,13 @@
+/*
+ * util.h
+ *
+ *  Created on: 2020. 8. 17.
+ *      Author: Kiwon
+ */
+
+#ifndef INC_UTIL_H_
+#define INC_UTIL_H_
+
+void	int2str(int n,char* buf,int len);
+
+#endif /* INC_UTIL_H_ */
